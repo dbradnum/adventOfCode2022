1b2e77aaf7bb319838c4fec58ca75197325bd154
David Bradnum <dbradnum@gmail.com>
2022-12-10 22:12:10 +0000

    day 10 solutions

---
+++ day10/day10.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+raw = read_csv("day10/day10.txt",col_names = "line") %>% 
+  separate(line,into = c("operation","value"),sep = " ", fill = "right") %>% 
+  mutate(value = as.numeric(value),
+         cycles = if_else(operation == "noop",1,2),
+         cycleNumber = cumsum(cycles)) 
+
+
+xVals = raw %>% 
+  complete(cycleNumber = seq(1,max(cycleNumber))) %>% 
+  mutate(value = coalesce(value,0)) %>% 
+  mutate(x = 1 + cumsum(value),
+         x = x - ifelse(!is.na(operation) & operation == "addx", value,0)) 
+
+
+
+# part 1 ------------------------------------------------------------------
+
+xVals %>% 
+  filter(cycleNumber %in% c(20,60,100,140,180,220) )%>% 
+  summarise(result = sum(cycleNumber * x ))
+
+
+# part 2  -----------------------------------------------------------------
+
+pixels = xVals %>% 
+  mutate(lit = ((x <= cycleNumber%%40) & (x >= cycleNumber%%40 - 2)))
+
+out = ""
+for (i in 1:nrow(pixels)){
+  out = paste0(out,ifelse(pixels$lit[i],"#","."))
+  if (i%%40 == 0) out = paste0(out,"\n")
+}
+writeLines(out)
